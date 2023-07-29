6b5c826720a338a7b19884c41b9e55d82d3bc795
akullyot <akullyot@gmail.com>
2023-07-29 10:57:49 -0500

    added a refresh button

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter');
+    let JSONresponse = await response.json();
+    var countValue = JSONresponse.value;
 
     function increment(){
         countValue++;
@@ -14,9 +17,15 @@ function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
+    function refresh()
+    {
+        countValue = JSONresponse.value;
+        countContainer.textContent = countValue;
+    }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
