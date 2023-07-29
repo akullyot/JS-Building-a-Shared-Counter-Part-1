a285ee239628c0fe161968fdaf1affdd7ed0a78d
akullyot <akullyot@gmail.com>
2023-07-29 12:01:28 -0500

    updating via patch

--- public/index.js
+++ public/index.js
@@ -3,29 +3,65 @@ async function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
     const refreshButton = document.querySelector('#refresh-button');
+    const resetButton = document.querySelector('#reset-button');
+
 
     let response = await fetch('http://localhost:9001/counter');
     let JSONresponse = await response.json();
-    var countValue = JSONresponse.value;
+    let countValue = JSONresponse.value;
+    const startingValue = JSONresponse.value;
+    function patchFetch()
+    {
+        //Increment in the db.json
+        fetch('http://localhost:9001/counter', 
+        {
+            method: 'PATCH',
+            headers: 
+            {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify
+            (
+                {
+                    value: countValue
+                }
+            )
+        });
+        console.log('counter.value changed to ' + countValue);
+    }
+     function increment()
+    {
 
-    function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        patchFetch();
     }
 
-    function decrement(){
+     function decrement()
+    {
         countValue--;
         countContainer.textContent = countValue;
+        patchFetch();
     }
-    function refresh()
+     function refresh()
     {
         countValue = JSONresponse.value;
         countContainer.textContent = countValue;
+        patchFetch()
+        alert("I do nothing now since we are patching db.json")
+    }
+    function reset()
+    {
+        countValue = startingValue;
+        countContainer.textContent = countValue;
+        patchFetch()
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     refreshButton.addEventListener('click', refresh);
+    resetButton.addEventListener('click', reset);
     countContainer.textContent = countValue;
 }
-main()
+//Run the JS
+main();
